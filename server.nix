8a327109cf1901fb61d4850599383815d1de0f4b
System administrator <root@nixos.>
2025-01-09 19:19:16 +0000

    init 2

---
+++ server.nix
@@ -0,0 +1,21 @@
+{...} : 
+{
+	services.nginx = {
+	 enable = false;
+	 config = ''
+	 worker_processes 2;
+	 events {
+	  worker_connections 1024;
+	 }
+	 http {
+	 	server {
+			listen 0.0.0.0:8080 ;
+			server_name localhost;
+			location / {
+				return 200 "Hello";
+			}
+		}
+	 }
+	 '';
+	};
+}
