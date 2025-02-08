541037ff1d220ec4fcaf02e21109b27d886874d9
rtocs <scottmathiasrichards@gmail.com>
2025-02-08 18:15:07 +0000

    dont restart

--- services/game.nix
+++ services/game.nix
@@ -3,11 +3,10 @@
 	systemd.services.game = {
 		enable = false;
 		description = "game-server";
-		serviceConfig = {
 
+		serviceConfig = {
 			Type = "forking";
 			ExecStart = "/var/test/test";
-			Restart = "on-failure";	
 		};
 
 		wantedBy = [ "multi-user.target" ];
