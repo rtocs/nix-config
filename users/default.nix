c0bf23c2788f4c9c76651fa51fa163cd10ced83b
rtocs <scottmathiasrichards@gmail.com>
2025-05-27 19:58:17 +0000

    user packages

--- users/default.nix
+++ users/default.nix
@@ -2,12 +2,16 @@
 {
 	users.users.nixos = {
 		isNormalUser  = true;
-		# we only add executables
 		packages = with pkgs; [
-			neovim
 			git
 			nmap
 			curl
+			gcc
+			gnumake
+			ripgrep
+			fd
+			unzip
+			neovim
 		];
 
 		home  = "/home/nixos";
@@ -15,5 +19,4 @@
 		extraGroups  = [ "wheel" "networkmanager" ];
 		openssh.authorizedKeys.keys  = [''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIz+Me+uWyR8naM4TBp+pLkawigVQkt6KxG+HWrVc0N''];
 	};
-
 }
