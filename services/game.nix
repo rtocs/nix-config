082091ba1ccd0cdfec92bb61cce7296e6d89c3a4
rtocs <scottmathiasrichards@gmail.com>
2025-06-08 13:09:01 +0000

    test server

--- services/game.nix
+++ services/game.nix
@@ -1,34 +1,19 @@
 {...} : 
 {
-	systemd.services.game = {
-		enable = false;
-		description = "game-server";
+  systemd.services.test-server = {
+    description = "test";
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
 
-		serviceConfig = {
-			Type = "forking";
-			ExecStart = "/var/test/test";
-		};
+    serviceConfig = {
+      ExecStart = "/home/nixos/test/main";
+      Restart = "always";
+    };
 
-		wantedBy = [ "multi-user.target" ];
-		after = [ "network-up.target" ];
-	};
+    # Optional: open the port via firewall
+    wantedBy = [ "multi-user.target" ];
+  };
+  networking.firewall.allowedTCPPorts = [ 8090 ];
 }
-# {
-#   systemd.services.my-web-server = {
-#     description = "My Simple Web Server";
-#     wantedBy = [ "multi-user.target" ];
-#     after = [ "network.target" ];
-#
-#     serviceConfig = {
-#       ExecStart = "/run/current-system/sw/bin/python3 -m http.server 8080";
-#       Restart = "always";
-#       User = "nobody"; # or a dedicated user
-#       WorkingDirectory = "/var/www"; # or another directory with content
-#     };
-#
-#     # Optional: open the port via firewall
-#     wantedBy = [ "multi-user.target" ];
-#   };
-#
-#   networking.firewall.allowedTCPPorts = [ 8080 ];
-# }
+
