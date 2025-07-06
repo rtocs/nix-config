09bdd28dd5f4b2f5616129ea63a6455fad018329
rtocs <scottmathiasrichards@gmail.com>
2025-07-06 12:26:46 +0000

    fix

--- flake.nix
+++ flake.nix
@@ -5,8 +5,7 @@
 		disko.url = "github:nix-community/disko";
 		disko.inputs.nixpkgs.follows = "nixpkgs";
 		nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-
-	        secrets.url = "git+ssh://git@github.com/you/secrets.git";
+		# secrets.url = "git+ssh://git@github.com/you/secrets.git";
 	};
 
 	outputs = {
@@ -19,21 +18,21 @@
 		nixosConfigurations.wsl = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			modules = [
-				nixos-wsl.nixosModules.default
-				{
-					system.stateVersion = "24.05";
-					wsl.enable = true;
-				}
-				./wsl/configuration.nix
-				./users/default.nix
+			nixos-wsl.nixosModules.default
+			{
+				system.stateVersion = "24.05";
+				wsl.enable = true;
+			}
+			./wsl/configuration.nix
+			./users/default.nix
 			];
 		};
 
 		nixosConfigurations.digitalocean = nixpkgs.lib.nixosSystem {
 			system = "x86_64-linux";
 			modules = [
-				disko.nixosModules.disko
-				{ disko.devices.disk.disk1.device = "/dev/vda"; }
+			disko.nixosModules.disko
+			{ disko.devices.disk.disk1.device = "/dev/vda"; }
 			{
 				# do not use DHCP, as DigitalOcean provisions IPs using cloud-init
 				networking.useDHCP = nixpkgs.lib.mkForce false;
@@ -41,9 +40,10 @@
 					enable = true;
 					network.enable = true;
 				};
-				./digitalOceanDroplet/configuration.nix
-				./users/server.nix
-				./services/game.nix
+			}
+			./digitalOceanDroplet/configuration.nix
+			./users/server.nix
+			./services/game.nix
 			];
 		};
 	};
