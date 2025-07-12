096cb405d5afc888047789094e3db4fed2fe1d07
rtocs <scottmathiasrichards@gmail.com>
2025-07-12 11:20:46 +0000

    nginx set user

--- services/nginx.nix
+++ services/nginx.nix
@@ -1,21 +1,25 @@
 { secrets, ... }:
 {
+	# users.users.nginx = {
+	# };
+
 	services.nginx = {
+		user = "nginx"
 		enable = true;
 
 		recommendedGzipSettings = true;
 		recommendedProxySettings = true;
 		recommendedTlsSettings = true;
 
-		virtualHosts."example.com" = {
-			enableACME = true;
-			forceSSL = true;
-
-			locations."/" = {
-				root = "/var/www/example"; 
-				index = "index.html";
-			};
-		};
+	# virtualHosts. = {
+	# 	enableACME = true;
+	# 	forceSSL = true;
+	#
+	# 	locations."/" = {
+	# 		root = "/var/www/example"; 
+	# 		index = "index.html";
+	# 	};
+	# };
 	};
 
 	security.acme = {
