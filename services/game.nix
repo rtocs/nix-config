b874cf9d95b20601cecd0e21e66b2aa6c4a90d2a
rtocs <scottmathiasrichards@gmail.com>
2025-07-19 11:51:51 +0000

    tls

--- services/game.nix
+++ services/game.nix
@@ -1,6 +1,6 @@
 {...} :
 {
-	systemd.services.test-server = {
+	services."game-server" = {
 		description = "test";
 		enable = true;
 		wantedBy = [ "multi-user.target" ];
@@ -10,9 +10,10 @@
 			ExecStart = "/home/nixos/test/main";
 			WorkingDirectory = "/home/nixos/test/"; 
 			Restart = "always";
-			User = "nixos";
+			User = "gameServer";
 		};
 	}
+
 	networking.firewall.allowedTCPPorts = [ 8090 ];
 }
 
--- services/nginx.nix
+++ services/nginx.nix
@@ -1,31 +1,29 @@
 { secrets, pkgs, ... } :
 {
-
-# users.users.nginx = {
-# };
 	services.nginx = {
 		enable = true;
 		virtualHosts.${secrets.testDomain} = {
-			# enableACME = true;
-			# forceSSL = true;
+			enableACME = true;
+			forceSSL = true;
 
 			root = pkgs.runCommand "hello-world-site" {} ''
 				mkdir -p $out
 				echo "go away" > $out/index.html
 				'';
 
-			locations."/" = {
-				extraConfig = ''
-					index index.html;
-				'';
+			locations = {
+				"/" = {
+					extraConfig = ''index index.html;'';
+				};
 			};
+
 		};
 	};
 
-	# security.acme = {
-	# 	acceptTerms = true;
-	# 	defaults.email = secrets.mainEmail:
-	# };
+	security.acme = {
+		acceptTerms = true;
+		defaults.email = secrets.mainEmail;
+	};
 
-	networking.firewall.allowedTCPPorts = [ 80 ];
+	networking.firewall.allowedTCPPorts = [ 80 443 ];
 }
