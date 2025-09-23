4b42a85ea5db71accfef2e920b7bdfedb309b7c8
rtocs <scottmathiasrichards@gmail.com>
2025-09-23 08:57:08 +0000

    nvim config

--- users/pkgs-overrides/nvim/init.lua
+++ users/pkgs-overrides/nvim/init.lua
@@ -97,7 +97,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
 		end
 
-		map('<leader>a', vim.lsp.buf.code_action, 'Code [A]ction', { 'n', 'x' })
+		map('<leader>ca', vim.lsp.buf.code_action, 'Code [A]ction', { 'n', 'x' })
 		map('<leader>f', vim.lsp.buf.format, 'format buffer', { 'n' })
 
 		map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
@@ -230,7 +230,7 @@ require 'nvim-treesitter.configs'.setup {
 	highlight = {
 		enable = true,
 		disable = function(_, buf)
-			local max_filesize = 100 * 1024
+			local max_filesize = 300 * 1024
 			local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
 			if ok and stats and stats.size > max_filesize then
 				return true
