bff6c0eb517e858b8f30a317eecf174f07fa38f3
rtocs <scottmathiasrichards@gmail.com>
2026-03-14 14:43:48 +0000

    update lock

--- users/pkgs-envs/pkgs-overrides/nvim/init.lua
+++ users/pkgs-envs/pkgs-overrides/nvim/init.lua
@@ -248,21 +248,6 @@ vim.lsp.config('elixirls', {
 })
 vim.lsp.enable('elixirls')
 
-require 'nvim-treesitter.configs'.setup {
-	highlight = {
-		enable = true,
-		disable = function(_, buf)
-			local max_filesize = 300 * 1024
-			local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-				return true
-			end
-		end,
-		additional_vim_regex_highlighting = false,
-	},
-}
-
-
 require("oil").setup()
 vim.keymap.set('n', '<leader>o', '<cmd>Oil<CR>', { desc = 'oil' })
 
